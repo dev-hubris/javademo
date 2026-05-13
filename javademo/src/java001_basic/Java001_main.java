b68a3a0d669d93d1b687b819972449a7a08fcd77
phb <dev.hubris@gmail.com>
2026-05-13 10:30:46 +0900

    ADD 20260513 First

---
+++ javademo/src/java001_basic/Java001_main.java
@@ -0,0 +1,10 @@
+package java001_basic;
+
+public class Java001_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		
+	}
+}
